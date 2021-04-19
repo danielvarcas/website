bb6a63103c9272d8c9071f7c0137fbea3b7dc862
Daniel Varcas <daniel.varcas@gmail.com>
2021-04-19 19:57:25 +0100

    Initialise Next app with TypeScript

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>My Website</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Welcome to my website!
+        </h1>
+      </main>
+    </div>
+  )
+}
