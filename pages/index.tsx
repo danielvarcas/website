46d079c4aa01dbe290daf5cd66f671614632a32e
Daniel Varcas <daniel.varcas@gmail.com>
2021-04-20 19:39:54 +0100

    add Tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home(): JSX.Element {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>My Website</title>
         <link rel="icon" href="/favicon.ico" />
