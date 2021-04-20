b0311fc9c148366f23c24ffc827f6d296cbeeab3
Daniel Varcas <daniel.varcas@gmail.com>
2021-04-20 23:34:25 +0100

    Remove cards, add some text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
 import Head from "next/head";
 import Link from "next/link";
 import Button from "../components/Button";
-import Card from "../components/Card";
 import Navbar from "../components/Navbar";
 
 export default function Home(): JSX.Element {
-  const lorem =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut laoreet ipsum neque. Vivamus ultrices sapien et velit facilisis iaculis. Integer venenatis, nulla lobortis rhoncus elementum, nisi mauris pulvinar purus, in cursus turpis dolor a arcu.";
   return (
     <div className="min-h-screen bg-gray-100">
       <Head>
@@ -20,26 +17,15 @@ export default function Home(): JSX.Element {
         </Link>
       </Navbar>
 
-      <div className="container mx-auto">
-        <main>
+      <div className="container  mx-auto">
+        <main className="text-lg">
           <h1 className="text-9xl">Hello World</h1>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 my-3">
-            <Card title="I am a title">
-              <p>{lorem}</p>
-            </Card>
-            <Card title="I am a title">
-              <p>{lorem}</p>
-            </Card>
-            <Card title="I am a title">
-              <p>{lorem}</p>
-            </Card>
-            <Card title="I am a title">
-              <p>{lorem}</p>
-            </Card>
-            <Card title="I am a title">
-              <p>{lorem}</p>
-            </Card>
+          <div className="my-3">
+            <p>
+              I&apos;m Daniel. I build high quality, accessible web applications
+              using cutting-edge technology.
+            </p>
           </div>
 
           <Button>Click me</Button>
